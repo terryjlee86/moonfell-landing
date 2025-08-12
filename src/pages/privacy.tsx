8a277d19782a0282e9cd38e6e3d7bf25b8ba62ce
terryjlee86 <77114262+terryjlee86@users.noreply.github.com>
2025-08-12 15:28:13 +0100

    Update privacy.tsx

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,1 +1,8 @@
-<main><h1>Privacy</h1><p>This is a placeholder privacy policy for Moonfell. Replace with your own.</p></main>
+export default function Privacy() {
+  return (
+    <main>
+      <h1>Privacy</h1>
+      <p>This is a placeholder privacy policy for Moonfell. Replace with your own.</p>
+    </main>
+  );
+}
