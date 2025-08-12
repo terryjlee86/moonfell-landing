9c1e5204bc69fb04cfeddf2f45736fb7998cf678
terryjlee86 <77114262+terryjlee86@users.noreply.github.com>
2025-08-12 15:28:29 +0100

    Update terms.tsx

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,1 +1,8 @@
-<main><h1>Terms</h1><p>These are placeholder terms for Moonfell. Replace with your own.</p></main>
+export default function Terms() {
+  return (
+    <main>
+      <h1>Terms</h1>
+      <p>These are placeholder terms for Moonfell. Replace with your own.</p>
+    </main>
+  );
+}
