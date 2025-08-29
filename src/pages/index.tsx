477efe27a3c65a387910ddf533a5b656a9669105
terryjlee86 <77114262+terryjlee86@users.noreply.github.com>
2025-08-29 15:21:01 +0100

    Updated hero copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,7 +152,7 @@ export default function Home() {
               draggable={false}
             />
             <h1 className="mt-4 text-3xl sm:text-4xl md:text-5xl font-extrabold leading-tight text-white">
-              Write your legend into the wilds.
+              A new world to explore with limitless possibilities, join the community below to follow development and the open beta.
             </h1>
             {/* Removed "Limitless actions..." paragraph */}
           </div>
